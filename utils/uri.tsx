1994396bd0aa7ce2ddc7add80983aa5b1e5dd12f
jaspreet@razorse.com-at-099391646783 <porwalsachin2510@gmail.com>
2024-10-24 15:11:53 +0530

    production ready app

--- utils/uri.tsx
+++ utils/uri.tsx
@@ -1,4 +1,6 @@
-export const SERVER_URI = "http://192.168.50.183:8000/api/v1";
+export const SERVER_URI = "https://pet-app-8scj.onrender.com/api/v1";
     
-//"https://pet-app-xf1n.onrender.com/api/v1";
+//"http://192.168.50.183:8000/api/v1";
+    
+
 
