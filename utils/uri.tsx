510e08fe58ba84a3fd164ab14242af1aa226f4e4
jaspreet@razorse.com-at-099391646783 <porwalsachin2510@gmail.com>
2024-11-04 12:16:47 +0530

    new backend url

--- utils/uri.tsx
+++ utils/uri.tsx
@@ -1,4 +1,4 @@
-export const SERVER_URI = "https://pet-app-2-2cbz.onrender.com/api/v1";
+export const SERVER_URI = "https://pet-app-3-pafg.onrender.com/api/v1";
     
 //"http://192.168.38.182:8000/api/v1";
     
