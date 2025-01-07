3e57a1e31a7f4e23ae6808e98171a7c693002d27
jaspreet@razorse.com-at-099391646783 <porwalsachin2510@gmail.com>
2025-01-07 12:10:50 +0530

    new url

--- utils/uri.tsx
+++ utils/uri.tsx
@@ -1,4 +1,4 @@
-export const SERVER_URI = "https://pet-app-s5ce.onrender.com/api/v1";
+export const SERVER_URI = "https://pet-app-4oso.onrender.com/api/v1";
     
 //"http://192.168.19.182:8000/api/v1";
     
